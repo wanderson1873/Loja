7e529b114d1763e3ea3f4c309ce10d9fdd1c96a0
Wanderson Oliveira <wanderson1873@live.com>
2021-04-01 18:00:10 +0100

    Create

---
+++ web/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '../styles/Footer.module.css'
+
+const Footer = () => {
+   return (
+      <footer className={style.footer}>
+         <p>By Wanderson O.</p>
+      </footer>
+   )
+}
+
+export default Footer
